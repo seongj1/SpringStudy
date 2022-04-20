d7a9643fd3078320f5736bfc04ed9afaf34aa462
DESKTOP-12AE4RE\dich2 <dich24@naver.com>
2022-04-20 13:02:49 +0900

    2022년 4월 20일 스프링수업

---
+++ src/main/java/com/spring/study/di/Main.java
@@ -0,0 +1,21 @@
+package com.spring.study.di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/*
+		 * ApplicationContext context = new
+		 * ClassPathXmlApplicationContext("com/spring/study/di/Context.xml");
+		 */
+		
+		/*
+		 * AuthService authService = (AuthService)context.getBean("authService");
+		 * authService.signin("seongjun", "1234");
+		 */
+		
+	}
+
+}
